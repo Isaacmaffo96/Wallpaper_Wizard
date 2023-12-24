6f4534e53739a669927c9bba1ff1a74c38fc6f27
isaacmaffo96 <isaacmaffeis@gmail.com>
2023-12-24 21:20:33 +0100

    setup pom.xml

--- src/main/java/org/wallpaperwizardapp/wallpaperwizard/Main.java
+++ src/main/java/org/wallpaperwizardapp/wallpaperwizard/Main.java
@@ -1,7 +1,12 @@
-package org.wallpaperwizardapp;
+package org.wallpaperwizardapp.wallpaperwizard;
+
+import org.wallpaperwizardapp.wallpaperwizard.model.Wallpaper;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Start");
+        Wallpaper wallpaper = new Wallpaper();
+        wallpaper.setWallpaper();
+
     }
 }
