812985c2e0b31b0ebfd3b8a0cdc0155479e983b5
isaacmaffo96 <isaacmaffeis@gmail.com>
2023-12-24 23:21:34 +0100

    change desktop wallpaper - ok

--- src/main/java/org/wallpaperwizardapp/wallpaperwizard/Main.java
+++ src/main/java/org/wallpaperwizardapp/wallpaperwizard/Main.java
@@ -1,12 +1,23 @@
 package org.wallpaperwizardapp.wallpaperwizard;
 
+import org.wallpaperwizardapp.wallpaperwizard.exceptions.PathDoesNotExistException;
 import org.wallpaperwizardapp.wallpaperwizard.model.Wallpaper;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Start");
+        System.out.println("[Main] Wallpaper Wizard Started");
+
+        Path imagePath = Paths.get("C:\\Users\\isaac\\Pictures\\Cars Theme\\Leclerc2022Monza.jpg");
+
         Wallpaper wallpaper = new Wallpaper();
-        wallpaper.setWallpaper();
+        try {
+            wallpaper.setWallpaper(imagePath);
+        } catch (PathDoesNotExistException e) {
+            e.printStackTrace();
+        }
 
     }
 }
---
+++ src/main/java/org/wallpaperwizardapp/wallpaperwizard/exceptions/PathDoesNotExistException.java
@@ -0,0 +1,7 @@
+package org.wallpaperwizardapp.wallpaperwizard.exceptions;
+
+public class PathDoesNotExistException extends Exception{
+    public PathDoesNotExistException(String message) {
+        super(message);
+    }
+}
