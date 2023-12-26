e4061d78999f9d7afd903d26c30e604a1150edef
isaacmaffo96 <isaacmaffeis@gmail.com>
2023-12-26 12:55:08 +0100

    add spring-boot

--- src/main/java/org/wallpaperwizardapp/wallpaperwizard/Main.java
+++ src/main/java/org/wallpaperwizardapp/wallpaperwizard/Main.java
@@ -3,15 +3,20 @@ package org.wallpaperwizardapp.wallpaperwizard;
 import org.wallpaperwizardapp.wallpaperwizard.exceptions.PathDoesNotExistException;
 import org.wallpaperwizardapp.wallpaperwizard.model.Wallpaper;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
+        System.out.println("------------------------------------------------------------------------");
         System.out.println("[Main] Wallpaper Wizard Started");
 
-        //Path imagePath = Paths.get("C:\\Users\\isaac\\Pictures\\Cars Theme\\Leclerc2021Turkish.jpg");
-        Path imagePath = Paths.get("C:\\Users\\isaac\\Pictures\\Cars Theme\\Leclerc2022Monza.jpg");
+        Path imagePath = Paths.get("C:\\Users\\isaac\\Pictures\\Cars Theme\\Leclerc2021Turkish.jpg");
+        //Path imagePath = Paths.get("C:\\Users\\isaac\\Pictures\\Cars Theme\\Leclerc2022Monza.jpg");
 
 
         Wallpaper wallpaper = new Wallpaper();
