a4eac1013737dfe34b19d01e46337f5f0c03583e
isaacmaffo96 <isaacmaffeis@gmail.com>
2023-12-26 17:31:22 +0100

    add nasa api Astronomy Picture of the Day

--- src/main/java/org/wallpaperwizardapp/wallpaperwizard/Main.java
+++ src/main/java/org/wallpaperwizardapp/wallpaperwizard/Main.java
@@ -1,10 +1,13 @@
 package org.wallpaperwizardapp.wallpaperwizard;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.wallpaperwizardapp.wallpaperwizard.exceptions.PathDoesNotExistException;
 import org.wallpaperwizardapp.wallpaperwizard.model.Wallpaper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.wallpaperwizardapp.wallpaperwizard.service.NasaAPIService;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -13,7 +16,7 @@ import java.nio.file.Paths;
 public class Main {
     public static void main(String[] args) {
 
-        SpringApplication.run(Main.class, args);
+        //SpringApplication.run(Main.class, args);
 
         System.out.println("------------------------------------------------------------------------");
         System.out.println("[Main] Wallpaper Wizard Started");
@@ -27,6 +30,24 @@ public class Main {
         } catch (PathDoesNotExistException e) {
             e.printStackTrace();
         }
+
+        ConfigurableApplicationContext context = SpringApplication.run(Main.class, args);
+
+        // Get the NASAImageService bean
+        NasaAPIService nasaImageService = context.getBean(NasaAPIService.class);
+
+        String imageUrl = null;
+        try {
+            imageUrl = nasaImageService.getImageOfTheDayUrl();
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Print the extracted image URL
+        System.out.println("Image URL: " + imageUrl);
+
+        // Close the Spring context
+        context.close();
 
     }
 }
