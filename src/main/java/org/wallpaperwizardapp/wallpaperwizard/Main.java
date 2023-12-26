61be2a5ff98845f7d09669a06c3f0cf9d78a44a1
isaacmaffo96 <isaacmaffeis@gmail.com>
2023-12-26 13:26:06 +0100

    updated gitignore

--- src/main/java/org/wallpaperwizardapp/wallpaperwizard/Main.java
+++ src/main/java/org/wallpaperwizardapp/wallpaperwizard/Main.java
@@ -12,12 +12,14 @@ import java.nio.file.Paths;
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
+
+        SpringApplication.run(Main.class, args);
+
         System.out.println("------------------------------------------------------------------------");
         System.out.println("[Main] Wallpaper Wizard Started");
 
         Path imagePath = Paths.get("C:\\Users\\isaac\\Pictures\\Cars Theme\\Leclerc2021Turkish.jpg");
         //Path imagePath = Paths.get("C:\\Users\\isaac\\Pictures\\Cars Theme\\Leclerc2022Monza.jpg");
-
 
         Wallpaper wallpaper = new Wallpaper();
         try {
